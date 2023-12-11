dd116081af1528f2fea511646acc909919dd21b9
IliasDrysdale <iliasdrysdale@gmail.com>
2023-12-11 18:17:42 +0100

    Joined the message in a console log and fixed a bug with a comma in the messageContent.

--- message.js
+++ message.js
@@ -1,4 +1,4 @@
-const messageContent = ['Work as hard as you can', 'Don\'t give up', 'You can do it', 'Only losers give up', 'Look in the mirror, do you see a loser?'];
+const messageContent = ['Work as hard as you can', 'Don\'t give up', 'You can do it', 'Only losers give up', 'Look in the mirror\, do you see a loser?'];
 const randomMessage = [];
 
 const randomNumberGenerator = () => {
@@ -9,3 +9,7 @@ for (i = 0; i < 3; i++) {
     let messageIndex = randomNumberGenerator();
     randomMessage.push(messageContent[messageIndex]);
 }
+
+randomMessage.join(' ');
+
+console.log(randomMessage.join(' '));
