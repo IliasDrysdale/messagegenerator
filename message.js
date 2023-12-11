e2592a2954d83861e6f475561a3cf5eeb931cd49
IliasDrysdale <iliasdrysdale@gmail.com>
2023-12-11 19:11:22 +0100

    Updated it so it doesn't produce 2 or more of the same string for the random message. Also improved the code a little bit.

--- message.js
+++ message.js
@@ -1,15 +1,18 @@
 const messageContent = ['Work as hard as you can', 'Don\'t give up', 'You can do it', 'Only losers give up', 'Look in the mirror\, do you see a loser?'];
 const randomMessage = [];
+const usedIndices = new Set();
 
 const randomNumberGenerator = () => {
-    return Math.floor(Math.random() * 5);
+    return Math.floor(Math.random() * messageContent.length);
 }
 
 for (i = 0; i < 3; i++) {
-    let messageIndex = randomNumberGenerator();
-    randomMessage.push(messageContent[messageIndex]);
+    let messageIndex1 = randomNumberGenerator();
+    while (usedIndices.has(messageIndex1)) { // Check if the index has already been used
+        messageIndex1 = randomNumberGenerator();
+    }
+    usedIndices.add(messageIndex1);
+    randomMessage.push(messageContent[messageIndex1]);
 }
 
-randomMessage.join(' ');
-
-console.log(randomMessage.join(' '));
+console.log(randomMessage.join('. '));
