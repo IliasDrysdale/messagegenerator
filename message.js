bf6479e690b0faec8d5f2c22912f7b8962471570
IliasDrysdale <iliasdrysdale@gmail.com>
2023-12-11 18:05:44 +0100

    Created messageArray, a numberGenerator & a loop which takes 3 random numbers

--- message.js
+++ message.js
@@ -1,1 +1,11 @@
-console.log('test');
+const messageContent = ['Work as hard as you can', 'Don\'t give up', 'You can do it', 'Only losers give up', 'Look in the mirror, do you see a loser?'];
+const randomMessage = [];
+
+const randomNumberGenerator = () => {
+    return Math.floor(Math.random() * 5);
+}
+
+for (i = 0; i < 3; i++) {
+    let messageIndex = randomNumberGenerator();
+    randomMessage.push(messageContent[messageIndex]);
+}
